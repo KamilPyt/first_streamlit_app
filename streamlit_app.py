e4d767ea3382411815bc4d2b6094cbc85e573877
Kamil Pytlarz <105375558+KamilPyt@users.noreply.github.com>
2023-03-09 19:23:47 +0100

    Update streamlit_app.py
    deleting a json line,
    modifying a get api line (adding kiwi)

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+
 
 # unpacking data from json format
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
